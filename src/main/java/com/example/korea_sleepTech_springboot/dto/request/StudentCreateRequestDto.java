848564f564ea470b0e8daa07c0ace32b17a91cd5
chang0506 <j8970@naver.com>
2025-04-29 12:45:44 +0900

    0429 - springboot student

---
+++ src/main/java/com/example/korea_sleepTech_springboot/dto/request/StudentCreateRequestDto.java
@@ -0,0 +1,10 @@
+package com.example.korea_sleepTech_springboot.dto.request;
+
+import lombok.Getter;
+
+@Getter
+
+public class StudentCreateRequestDto {
+    private String name;
+    private String email;
+}
